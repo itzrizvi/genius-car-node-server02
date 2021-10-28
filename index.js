ef9de84825fde748e3b1ad75239bebc18af35f01
itzrizvi <shahriar.me.work@gmail.com>
2021-10-28 20:16:23 +0600

    upload server

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middle Ware
+app.use(cors());
+app.use(express.json());
+
+// DB CREDENTIALS
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w9ewo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("genius_car_practice_data");
+        const servicesCollection = database.collection("services");
+        console.log('DB CONNECTED');
+
+        // SINGLE API FOR ARRAY ON PORT 5000
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const servicesData = await cursor.toArray();
+            res.send(servicesData);
+        });
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        });
+
+        // GET API FOR SINGLE SERVICE
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        });
+
+        // DLETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        });
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hellow Genius Car Practice!!');
+})
+
+app.listen(port, () => {
+    console.log(`Listening to PORT NO - ${port}`);
+})
